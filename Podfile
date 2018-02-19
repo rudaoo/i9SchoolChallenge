ac93c71f75f0cb6c159468d1edebb5e74d698bfb
Rudney de Camargo <rudneycamargo@brq.com>
2018-02-19 11:12:12 -0300

    instalando cocoapod e crashLitycs

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, ’11.0’
+
+inhibit_all_warnings!
+
+def shared_pods
+pod 'Crashlytics'
+end 
+
+target ‘i9School Challenges’ do
+    shared_pods
+end
---
+++ Pods/Target Support Files/Pods-i9School Challenges/Pods-i9School Challenges-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_i9School_Challenges : NSObject
+@end
+@implementation PodsDummy_Pods_i9School_Challenges
+@end
