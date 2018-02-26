138d1963c50f8338fc83140597a9f75ca3ff7ebf
Rudney de Camargo <rudneycamargo@brq.com>
2018-02-26 13:09:54 -0300

    layout de tela de desafio

--- i9School Challenges/i9 Files/Challenge/Controller/ChallengeScreenViewController.m
+++ i9School Challenges/i9 Files/Challenge/Controller/ChallengeScreenViewController.m
@@ -19,6 +19,12 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
