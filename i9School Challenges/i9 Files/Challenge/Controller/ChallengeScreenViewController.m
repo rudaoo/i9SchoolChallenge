52f4a6e1c9417f6d01cc3cd58e88ffb99d23b976
Rudney de Camargo <rudneycamargo@brq.com>
2018-03-07 15:16:47 -0300

    mover as celulas com animacao

--- i9School Challenges/i9 Files/Challenge/Controller/ChallengeScreenViewController.m
+++ i9School Challenges/i9 Files/Challenge/Controller/ChallengeScreenViewController.m
@@ -21,8 +21,12 @@
 @property (weak, nonatomic) IBOutlet UIButton *moveBackButton;
 @property (weak, nonatomic) IBOutlet UIButton *turnRightButton;
 @property (weak, nonatomic) IBOutlet UIButton *turnLeftButton;
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
 
 @property (strong, nonatomic) NSMutableArray *actionsSequence;
+@property (strong, nonatomic) NSTimer *stopWatchTimer;
+@property (strong, nonatomic) NSDate *startDate;
+@property (nonatomic) BOOL challengeRunning;
 
 
 @end
@@ -34,13 +38,17 @@
     [self registerCellNibs];
     self.actionsSequence = [[NSMutableArray alloc]init];
     
+    UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc]
+                                               initWithTarget:self action:@selector(longPressGestureRecognized:)];
+    [self.tableView addGestureRecognizer:longPress];
+
+    
     self.circleHintView.layer.borderColor = [[UIColor whiteColor] CGColor];
     self.circleHintView2.layer.borderColor = [[UIColor whiteColor] CGColor];
     self.circleHintView3.layer.borderColor = [[UIColor whiteColor] CGColor];
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
-    [self.tableView setEditing:YES];
     [self.tableView reloadData];
-    // Do any additional setup after loading the view.
+
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -89,26 +97,6 @@
     return cell;
 }
 
-- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return UITableViewCellEditingStyleNone;
-}
-
-- (BOOL)tableView:(UITableView *)tableview shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath *)indexPath {
-    return NO;
-}
-
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    
-    return YES;
-}
-
--(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
-    
-    NSString *stringToMove = [self.actionsSequence objectAtIndex:sourceIndexPath.row];
-    [self.actionsSequence removeObjectAtIndex:sourceIndexPath.row];
-    [self.actionsSequence insertObject:stringToMove atIndex:destinationIndexPath.row];
-}
-
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     return UITableViewAutomaticDimension;
@@ -131,15 +119,25 @@
         
         [sender setSelected:YES];
         [self.buildButtonLabel setText:@"Parar"];
+        self.challengeRunning = YES;
+        [self startTimer];
+        [self finishChallenge];
+
     }
     else {
         
         [sender setSelected:NO];
         [self.buildButtonLabel setText:@"Rodar"];
+        self.challengeRunning = NO;
+        [self stopTimer];
     }
 }
 
 - (IBAction)onButtonCarActionPressed:(UIButton *)sender {
+    
+    if (!self.challengeRunning) {
+        return;
+    }
     
     if (sender == self.moveFowardButton) {
         
@@ -180,6 +178,10 @@
 
 -(void) eraseCarAction:(UIButton*) sender {
     
+    if (!self.challengeRunning) {
+        return;
+    }
+    
     CGPoint buttonPosition = [sender convertPoint:CGPointZero toView:self.tableView];
     NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:buttonPosition];
     NSIndexPath *newindexPath = [NSIndexPath indexPathForRow:indexPath.row - 1 inSection:0];
@@ -214,6 +216,179 @@
         });
     }
     
+}
+
+
+
+- (IBAction)longPressGestureRecognized:(id)sender {
+    
+    if (!self.challengeRunning) {
+        return;
+    }
+    
+    
+    UILongPressGestureRecognizer *longPress = (UILongPressGestureRecognizer *)sender;
+    UIGestureRecognizerState state = longPress.state;
+    
+    CGPoint location = [longPress locationInView:self.tableView];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:location];
+    
+    static UIView       *snapshot = nil;        ///< A snapshot of the row user is moving.
+    static NSIndexPath  *sourceIndexPath = nil; ///< Initial index path, where gesture begins.
+    
+    switch (state) {
+        case UIGestureRecognizerStateBegan: {
+            if (indexPath) {
+                sourceIndexPath = indexPath;
+                
+                UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+                
+                // Take a snapshot of the selected row using helper method.
+                snapshot = [self customSnapshoFromView:cell];
+                
+                // Add the snapshot as subview, centered at cell's center...
+                __block CGPoint center = cell.center;
+                snapshot.center = center;
+                snapshot.alpha = 0.0;
+                [self.tableView addSubview:snapshot];
+                [UIView animateWithDuration:0.25 animations:^{
+                    
+                    // Offset for gesture location.
+                    center.y = location.y;
+                    snapshot.center = center;
+                    snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05);
+                    snapshot.alpha = 0.98;
+                    cell.alpha = 0.0;
+                    cell.hidden = YES;
+                    
+                }];
+            }
+            break;
+        }
+            
+        case UIGestureRecognizerStateChanged: {
+            CGPoint center = snapshot.center;
+            center.y = location.y;
+            snapshot.center = center;
+            
+            // Is destination valid and is it different from source?
+            if (indexPath && ![indexPath isEqual:sourceIndexPath]) {
+                
+                // ... update data source.
+                [self.actionsSequence exchangeObjectAtIndex:indexPath.row withObjectAtIndex:sourceIndexPath.row];
+                
+                // ... move the rows.
+                [self.tableView moveRowAtIndexPath:sourceIndexPath toIndexPath:indexPath];
+                
+                // ... and update source so it is in sync with UI changes.
+                sourceIndexPath = indexPath;
+            }
+            break;
+        }
+            
+        default: {
+            // Clean up.
+            UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:sourceIndexPath];
+            cell.alpha = 0.0;
+            
+            [UIView animateWithDuration:0.25 animations:^{
+                
+                snapshot.center = cell.center;
+                snapshot.transform = CGAffineTransformIdentity;
+                snapshot.alpha = 0.0;
+                cell.alpha = 1.0;
+                
+            } completion:^(BOOL finished) {
+                
+                cell.hidden = NO;
+                sourceIndexPath = nil;
+                [snapshot removeFromSuperview];
+                snapshot = nil;
+                [self.tableView reloadData];
+                
+            }];
+            
+            break;
+        }
+    }
+}
+
+
+-(void) finishChallenge {
+    
+    NSLog(@"%@", self.actionsSequence);
+    NSLog(@"%@", self.timerLabel.text);
+}
+
+
+- (void)updateTimer {
+    // Create date from the elapsed time
+    NSDate *currentDate = [NSDate date];
+    NSTimeInterval timeInterval = [currentDate
+                                   timeIntervalSinceDate:self.startDate];
+    NSLog(@"time interval %f",timeInterval);
+    
+    NSDate *timerDate = [NSDate
+                         dateWithTimeIntervalSince1970:timeInterval];
+    
+    // Create a date formatter
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"mm:ss"];
+    [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
+    
+    
+    // Format the elapsed time and set it to the label
+    NSString *timeString = [dateFormatter stringFromDate:timerDate];
+    self.timerLabel.text = timeString;
+}
+
+
+#pragma mark - Helper methods
+
+/** @brief Returns a customized snapshot of a given view. */
+- (UIView *)customSnapshoFromView:(UIView *)inputView {
+    
+    // Make an image from the input view.
+    UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, NO, 0);
+    [inputView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    // Create an image view.
+    UIView *snapshot = [[UIImageView alloc] initWithImage:image];
+    snapshot.layer.masksToBounds = NO;
+    snapshot.layer.cornerRadius = 0.0;
+    snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
+    snapshot.layer.shadowRadius = 5.0;
+    snapshot.layer.shadowOpacity = 0.4;
+    
+    return snapshot;
+}
+
+- (void)stopTimer
+{
+    [self.stopWatchTimer invalidate];
+    self.stopWatchTimer = nil;
+    [self updateTimer];
+    
+}
+
+- (void)startTimer
+{
+    
+    if (self.stopWatchTimer) {
+        [self.stopWatchTimer invalidate];
+        self.stopWatchTimer = nil;
+    }
+    
+    self.startDate = [NSDate date];
+    
+    // Create the stop watch timer that fires every 100 ms
+    self.stopWatchTimer = [NSTimer scheduledTimerWithTimeInterval:1.0/10.0
+                                                           target:self
+                                                         selector:@selector(updateTimer)
+                                                         userInfo:nil
+                                                          repeats:YES];
 }
 
 
